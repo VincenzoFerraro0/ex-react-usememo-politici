421d5503b6358be33c96e83291c50f5eed0e4f15
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-06-04 11:14:23 +0200

    milestone 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-
-
+import { useState, useEffect } from "react"
+import './index.css';
 function App() {
-  
+
+  //dichiaro lo stato iniziale dell'oggetto 
+  const [politicians, setPoliticians] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/politicians')
+      .then(res => res.json())
+      .then(data => setPoliticians(data))
+      .catch(error => console.error(error))
+  }, []) // useEffect vuole sempre una dipendenza che adesso è settata come vuota
+
+
+
+  console.log(politicians)
   return (
     <>
-     
+      <h1>Lista dei politici</h1>
+      <div className="politicians-list">
+        {politicians.map(p => {
+          const { id, name, image, position, biography } = p
+          return (
+            <div key={id} className="politicians-card">
+              <h3 className="card-title">{name}</h3>
+              <figure className="card-image">
+                <img src={image} alt={name} />
+              </figure>
+              <p className="card-position"><strong>posizione:</strong>{position}</p>
+              <p className="card-biography">{biography}</p>
+            </div>
+          )
+        })
+        }
+      </div>
     </>
   )
 }
